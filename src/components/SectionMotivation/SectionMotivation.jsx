f5198c765b092ad404ac5efad087c0c74d34c508
Nebelschwimmer <nebelschwimmer@gmail.com>
2024-01-05 21:06:39 +0300

    v2

--- src/components/SectionMotivation/SectionMotivation.jsx
+++ src/components/SectionMotivation/SectionMotivation.jsx
@@ -7,7 +7,8 @@ export const SectionMotivation = forwardRef((props, ref) => {
     <section ref={ref}  className='motivation'>
       
       <div  className='motivation__text__wrapper'>
-          <h1 className='motivation__text__header'>Осмельтесь на большее: Преобразите свои амбиции в реальность с нами!</h1>
+          <h1 className='motivation__text__header'>Осмельтесь на большее:</h1>
+          <h1 className='motivation__text__header'>Преобразите свои амбиции в реальность с нами!</h1>
           <p>Присоединяйтесь к DANDELION: где амбиции встречают реальность.</p>
           <p>Здесь вы не просто занимаетесь бизнесом –</p>
           <p className='motivation__text__emph'><b>ВЫ СОЗДАЕТЕ ИМПЕРИЮ!</b></p>
